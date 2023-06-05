de64065bf11f0a6add4dad63ff861faab21b1519
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-06-05 07:42:15 -0700

    fix typo

--- tvgolib.go
+++ tvgolib.go
@@ -878,8 +878,6 @@ func getTvShowInfo(apath string, tvshowpicPath string) (TvSI TVShowInfoS) {
 		log.Println("Starting BlackKnight")
 		fmt.Println(filename[18:boo])
 
-	
-
 	// /media/pi/PiTB/media/ TVShows/FooBar/s1/FooBar S01E01 Glorious Purpose.mp4
 	case strings.Contains(apath, "FooBar"):
 		_, filename := path.Split(apath)
@@ -892,8 +890,8 @@ func getTvShowInfo(apath string, tvshowpicPath string) (TvSI TVShowInfoS) {
 		TvSI.TvFSPath = fspath
 		TvSI.Catagory = "FooBar"
 		TvSI.Season = filename[8:10]
-		TvSI.Episode = filename[12:14]
-		TvSI.Title = filename[14:boo]
+		TvSI.Episode = filename[11:13]
+		TvSI.Title = filename[13:boo]
 		TvSI.Series = "FooBar"
 		log.Println("Starting FooBar")
 		fmt.Println(filename[14:boo])
